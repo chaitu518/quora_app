4b30f62fe68b87861573a488d367902d0dd37568
Naga Sri Sai Chaitanya <saichaitanya518@gmail.com>
2025-07-13 13:43:16 +0530

    answer controller added but need to add logic for sentimental analysis, changes are maded to question controller

--- controllers/answer_controller.py
+++ controllers/answer_controller.py
@@ -1,9 +1,24 @@
 from fastapi import APIRouter
+from schemas.answer_schema import AnswerCreate, AnswerOut
+from models import answer_model
+from bson import ObjectId
 
 router = APIRouter()
 
-@router.get("/all")
-def get_all_answers():
-    return{
-        "a":"hai"
+@router.post("/", response_model=AnswerOut)
+async def create(a: AnswerCreate):
+    sentiment = "positive"
+    data = {
+        "question_id": ObjectId(a.question_id),
+        "user_id": a.user_id,
+        "content": a.content,
+        "sentiment": sentiment
+    }
+    aid = await answer_model.create_answer(data)
+    return {
+        "id": aid,
+        "question_id": a.question_id,
+        "user_id": a.user_id,
+        "content": a.content,
+        "sentiment": sentiment
     }
--- controllers/question_controller.py
+++ controllers/question_controller.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter
-from utils.serilizer import serialize_question
+from utils.serilizer import serialize_question,serialize_answer
 from schemas.question_schema import QuestionCreate,QuestionOut
-from models import question_model
+from models import question_model,answer_model
 router = APIRouter()
 
 @router.post("/",response_model=QuestionOut)
@@ -14,8 +14,9 @@ async def list_all():
     questions = await question_model.get_all_questions()
     response = []
     for q in questions:
+        answers = await answer_model.get_answers_by_question(q["_id"])
         response.append({
             **serialize_question(q),
-            "answers": []
+            "answers": [serialize_answer(a) for a in answers]
         })
     return response
